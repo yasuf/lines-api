4bc18b4dc69b17b282b803f0b441916b2c7c2bb5
Yasu Flores <carlosyasu91@gmail.com>
2022-02-12 17:50:18 -0600

    figured out what to do next

--- src/api/line/controllers/line.js
+++ src/api/line/controllers/line.js
@@ -6,4 +6,12 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::line.line');
+module.exports = createCoreController('api::line.line', ({ strapi }) => ({
+  async find(context) {
+    const { data } = await super.find(context);
+    // TODO:
+    // 1. Setup S3 settings
+    // 2. Grab the AWS S3 url and get signed URLs for all the elements in the list
+    return { data };
+  }
+}));
